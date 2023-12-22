af7067a7e372e8f1489a423afde082206957c465
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-12-23 01:58:16 +0530

    Fix Spelling

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Kanishk's Dairy";
+export const SITE_TITLE = "Kanishk's Diary";
 export const SITE_DESCRIPTION =
-  "Welcome to my dairy! I write about Engineering ,Computer Science, Socail Problems and my Experiences with life.";
+  "Welcome to my diary! I write about Engineering ,Computer Science, Socail Problems and my Experiences with life.";
 export const TWITTER_HANDLE = "@itsKanishkP";
 export const MY_NAME = "Kanishk Pachauri";
 
